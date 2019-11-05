3f39243926c195536619117dc376dfb6fa63c69a
Lucien van Essen <2113629@talnet.nl>
2019-11-05 12:19:58 +0100

    second

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+function verander1(){
+    document.getElementById("img1").style.display="none"
+    document.getElementById("img2").style.display="block"
+}
+
+function verander2(){
+    document.getElementById("img2").style.display="none"
+    document.getElementById("img3").style.display="block"
+}
+
+function verander3(){
+    document.getElementById("img3").style.display="none"
+    document.getElementById("img1").style.display="block"
+}
