a9c32658817e0029af4258e86184505b4d85b87a
Lucien van Essen <2113629@talnet.nl>
2019-11-06 11:50:15 +0100

    Final commit

--- JS/script.js
+++ JS/script.js
@@ -12,3 +12,27 @@ function verander3(){
     document.getElementById("img3").style.display="none"
     document.getElementById("img1").style.display="block"
 }
+function verander4(){
+    document.getElementById("img4").style.display="none"
+    document.getElementById("img5").style.display="block"
+}
+function verander5(){
+    document.getElementById("img5").style.display="none"
+    document.getElementById("img6").style.display="block"
+}
+function verander6(){
+    document.getElementById("img6").style.display="none"
+    document.getElementById("img4").style.display="block"
+}
+function like(){
+    document.getElementById("likeButton").style.backgroundColor="green"
+    document.getElementById("dislikeButton").style.backgroundColor="black"
+}
+function dislike(){
+    document.getElementById("dislikeButton").style.backgroundColor="red"
+    document.getElementById("likeButton").style.backgroundColor="black"
+}
+
+datum = new Date()
+    document.getElementById("datum").innerHTML = datum.getDate() + "-" + (datum.getMonth()+1)
+        + "-" + datum.getFullYear();
